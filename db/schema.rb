04d317e734db4e1a968afd11d8e5182dcf0698db
stonefruit <jeromeng888@gmail.com>
2018-04-24 13:07:20 +0800

    add migration for events users join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_23_051205) do
+ActiveRecord::Schema.define(version: 2018_04_24_034111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2018_04_23_051205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "show"
+  end
+
+  create_table "events_users", id: false, force: :cascade do |t|
+    t.bigint "event_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["event_id", "user_id"], name: "index_events_users_on_event_id_and_user_id"
+    t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
   end
 
 end
