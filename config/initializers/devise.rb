0dc1848fcc41e1150ad5cabec4830722e861bb42
WalterWX <walter@DESKTOP-TMEIA6D.localdomain>
2018-04-24 14:10:41 +0800

    Devise and Linkedin Done, need to test on production site before beautifying

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,6 +258,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+	## To delete off later before publishing
+	config.omniauth :linkedin, "812316hwj1q2me", "9Zk7sM9o5UYuZoso"
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
