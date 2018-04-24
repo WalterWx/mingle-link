96e0c6bee55ab5aa5126dd2b75f12b8bacb2d896
Ticktock <smu@ticktock.local>
2018-04-24 10:43:19 +0800

    added jquery and formatted event form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require semantic_ui/semantic_ui
 //= require rails-ujs
 //= require activestorage
