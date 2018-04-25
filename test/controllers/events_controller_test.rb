0d1221f6519ce20e315464533e9d230f1e72e6f9
stonefruit <jeromeng888@gmail.com>
2018-04-25 11:17:33 +0800

    add first test for events controller

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save event without parameters" do
+    event = Event.new
+    assert_not event.save
+  end
+
+  # test "redirect user to login page if not signed in" do
+    
 end
