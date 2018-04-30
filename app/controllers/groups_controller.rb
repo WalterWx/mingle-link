046e38d02d17d648db458dd491e73ffe28e1b38a
stonefruit <jeromeng888@gmail.com>
2018-04-30 14:56:55 +0800

    rectify logic for joining joined group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -41,9 +41,24 @@ class GroupsController < ApplicationController
 
   def join_group
     @group = Group.find_by_id(params[:id])
-    if  GroupsUser.where("user_id = ? AND group_id = ?", current_user.id, @group.id).empty?
+    #event_groups = @group.event.groups
+    #user_groups = GroupsUser.where("user_id = ?", current_user.id)
+    #user_groups.each do |user_group|
+    #  puts user_group.group_id
+    #  if event_groups.find_by_id(user_group.group_id)
+    #    user_group.destroy
+    #  end
+
+    #end
+    #byebug
+
+    # check if user is in a particular group yet, double negative use as
+    # object is returned, and cannot be tested as false, even when empty
+    if not GroupsUser.where("user_id = ? AND group_id = ?", current_user.id, @group.id).empty?
+    else
       @groupuser = GroupsUser.create(user_id: current_user.id, group_id: @group.id)
     end
+
     redirect_to event_group_path(@group)
   end
 
