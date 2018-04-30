9090c086d916a47ab749bc4897881660a8d8ed23
stonefruit <jeromeng888@gmail.com>
2018-04-30 10:11:50 +0800

    add join_group method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,41 +1,33 @@
 class GroupsController < ApplicationController
 
- 	def index
-  	@groups = Group.all
+  def index
+    @groups = Group.all
   end
 
-	def new
+  def new
   end
 
   def show
-
-  	@group = Group.find(params[:id])
+    @group = Group.find(params[:id])
     @group.event_id = params[:event_id]
     @list = @group.users()
-
   end
 
   def edit
     @group = Group.find(params[:id])
-
   end
 
   def create
-    
     @event = Event.find(params[:event_id])
-  	@group = @event.groups.create(group_params)    
+    @group = @event.groups.create(group_params)    
     @groupuser = GroupsUser.create(user_id: current_user.id , group_id: @group.id)
-  	      File.write('a', @groupuser)
+    File.write('a', @groupuser)
 
     redirect_to event_path(@event)
-  	
-
   end
 
   def update
-    
     @group = Group.find(params[:id])
-   
     if(@group.update(group_params))
       redirect_to event_path(@group.event)
     end
@@ -50,10 +42,10 @@ class GroupsController < ApplicationController
 
   end
 
-private
+  private
 
-	def group_params
-		params.require(:group).permit(:title, :description, :id, :event_id)
-	end
+  def group_params
+    params.require(:group).permit(:title, :description, :id, :event_id)
+  end
 
 end
